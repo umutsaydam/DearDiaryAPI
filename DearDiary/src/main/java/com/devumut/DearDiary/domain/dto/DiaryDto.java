f63df3cf7bc877fd849e116ae3a99297f653423f
umutsaydam <umutsaydam24@gmail.com>
2025-04-09 15:30:49 +0300

    Add: DiaryDto, builder annotation was added

--- DearDiary/src/main/java/com/devumut/DearDiary/domain/dto/DiaryDto.java
+++ DearDiary/src/main/java/com/devumut/DearDiary/domain/dto/DiaryDto.java
@@ -1,6 +1,7 @@
 package com.devumut.DearDiary.domain.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class DiaryDto {
     private UUID diary_id;
     private Date diary_date;
