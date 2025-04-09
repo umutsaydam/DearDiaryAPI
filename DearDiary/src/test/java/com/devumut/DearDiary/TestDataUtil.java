1710b13822cd9387a59aa96ca402cf53db8a5a69
umutsaydam <umutsaydam24@gmail.com>
2025-04-09 15:36:59 +0300

    Add: Test data added

--- DearDiary/src/test/java/com/devumut/DearDiary/TestDataUtil.java
+++ DearDiary/src/test/java/com/devumut/DearDiary/TestDataUtil.java
@@ -1,6 +1,8 @@
 package com.devumut.DearDiary;
 
+import com.devumut.DearDiary.domain.dto.DiaryDto;
 import com.devumut.DearDiary.domain.dto.UserDto;
+import com.devumut.DearDiary.domain.entities.DiaryEntity;
 import com.devumut.DearDiary.domain.entities.UserEntity;
 
 public final class TestDataUtil {
@@ -23,6 +25,38 @@ public final class TestDataUtil {
                 .user_id(null)
                 .username("umut")
                 .password("password")
+                .build();
+    }
+
+    public static DiaryEntity getDiaryEntityA() {
+        return DiaryEntity.
+                builder()
+                .diary_content("Diary ContentA")
+                .diary_emotion(0)
+                .build();
+    }
+
+    public static DiaryDto getDiaryDtoA() {
+        return DiaryDto
+                .builder()
+                .diary_content("Diary ContentA")
+                .diary_emotion(0)
+                .build();
+    }
+
+    public static DiaryEntity getDiaryEntityB() {
+        return DiaryEntity.
+                builder()
+                .diary_content("Diary ContentB")
+                .diary_emotion(1)
+                .build();
+    }
+
+    public static DiaryDto getDiaryDtoB() {
+        return DiaryDto
+                .builder()
+                .diary_content("Diary ContentB")
+                .diary_emotion(1)
                 .build();
     }
 }
